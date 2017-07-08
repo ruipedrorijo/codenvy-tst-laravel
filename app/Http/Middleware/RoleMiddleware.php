ee3113741b524c66bf782cc31cd20f80fc4584e1
Rui Rijo <ruipedrorijo@hotmail.com>
2017-07-08 21:21:37 +0000

    nova versao do middleware-com a funcao terminate a funcionar

--- app/Http/Middleware/RoleMiddleware.php
+++ app/Http/Middleware/RoleMiddleware.php
@@ -6,13 +6,18 @@ use Closure;
 
 class RoleMiddleware {
    public function handle($request, Closure $next, $role) {
-      echo "Role 2 ". $role;
+      echo "Role 2 ". $role; 
       
-      /*
-       * 
-       */
-      
+      //if (! $request->user()->hasRole($role)) {
+            // Redirect...
+      //  }
       
       return $next($request);
+   }
+   
+   public function terminate($request, $response)
+   {
+      echo "I am at terminate function";
+        
    }
 }
